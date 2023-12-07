e40086d15a931c37b26a473565f498711338beab
alaminediassy <diassy.alamine@gmail.com>
2023-12-07 16:00:57 +0100

    first commit

---
+++ customer-service/src/main/java/com/nema/customerservice/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.nema.customerservice.repository;
+
+import com.nema.customerservice.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
